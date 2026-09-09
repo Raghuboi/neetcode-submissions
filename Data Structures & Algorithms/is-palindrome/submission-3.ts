58f6496efbea342b99a236bf3e202f3e7a812806
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2026-09-09 13:36:55 -0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let left = 0
+        let right = s.length - 1
+
+        while (left < right) {
+            while (left < right && !this.isAlphaNumeric(s[left])) {
+                left++
+            }
+            while (left < right && !this.isAlphaNumeric(s[right])) {
+                right--
+            }
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false
+            }
+
+            left++
+            right--
+        }
+
+        return true
+    }
+
+    isAlphaNumeric(s: string): boolean {
+        return ((s >= "A" && s <= "Z") || (s >= "a" && s <= "z") || (s >= "0" && s <= "9"))
+    }
+}
